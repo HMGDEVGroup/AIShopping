dd8f91b8f23e29989fef6bc509727c6841eee910
Patrick Henry <henrymediagroup@mac.com>
2026-01-20 20:11:16 -0500

    Initial Commit

---
+++ AIShopping/AIShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIShoppingApp.swift
+//  AIShopping
+//
+//  Created by Patrick on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct AIShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
